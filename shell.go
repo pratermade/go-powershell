469152eeee2e10153322958a74234eb951565cf8
pratermade <steve.prater@gmail.com>
2021-02-12 11:54:02 -0600

    complete POC

--- shell.go
+++ shell.go
@@ -5,10 +5,8 @@ package powershell
 import (
 	"fmt"
 	"io"
-	"strings"
 	"sync"
 
-	"github.com/juju/errors"
 	"github.com/pratermade/go-powershell.git/v2/backend"
 	"github.com/pratermade/go-powershell.git/v2/utils"
 )
@@ -27,7 +25,7 @@ type shell struct {
 	stderr io.Reader
 }
 
-func New(backend backend.Starter) (Shell, error) {
+func New(backend backend.Starter) (*shell, error) {
 	handle, stdin, stdout, stderr, err := backend.StartProcess("powershell.exe", "-NoExit", "-Command", "-")
 	if err != nil {
 		return nil, err
@@ -36,9 +34,11 @@ func New(backend backend.Starter) (Shell, error) {
 	return &shell{handle, stdin, stdout, stderr}, nil
 }
 
-func (s *shell) Execute(cmd string) (string, string, error) {
+func (s *shell) Execute(cmd string, stdout chan<- string, stderr chan<- string) {
 	if s.handle == nil {
-		return "", "", errors.Annotate(errors.New(cmd), "Cannot execute commands on closed shells.")
+		stderr <- fmt.Sprintf("cannot execute on closed shells: %s", cmd)
+		close(stderr)
+		close(stdout)
 	}
 
 	outBoundary := createBoundary()
@@ -49,26 +49,46 @@ func (s *shell) Execute(cmd string) (string, string, error) {
 
 	_, err := s.stdin.Write([]byte(full))
 	if err != nil {
-		return "", "", errors.Annotate(errors.Annotate(err, cmd), "Could not send PowerShell command")
+		stderr <- fmt.Sprintf("Could not send PowerShell command: %s\nError: %s", cmd, err.Error())
+		close(stderr)
+		close(stdout)
 	}
 
 	// read stdout and stderr
-	sout := ""
-	serr := ""
+	sout := make(chan string)
+	serr := make(chan string)
 
 	waiter := &sync.WaitGroup{}
 	waiter.Add(2)
 
-	go streamReader(s.stdout, outBoundary, &sout, waiter)
-	go streamReader(s.stderr, errBoundary, &serr, waiter)
+	go streamReader(s.stdout, outBoundary, sout)
+	go streamReader(s.stderr, errBoundary, serr)
 
+	// read and write stdout
+	go func() {
+		for {
+			stdoutLines, ok := <-sout
+			if !ok {
+				break
+			}
+			stdout <- stdoutLines
+		}
+		close(stdout)
+		waiter.Done()
+	}()
+	// read and write the stderr
+	go func() {
+		for {
+			stderrLines, ok := <-serr
+			if !ok {
+				break
+			}
+			stderr <- stderrLines
+		}
+		close(stderr)
+		waiter.Done()
+	}()
 	waiter.Wait()
-
-	if len(serr) > 0 {
-		return sout, serr, errors.Annotate(errors.New(cmd), serr)
-	}
-
-	return sout, serr, nil
 }
 
 func (s *shell) Exit() {
@@ -89,30 +109,41 @@ func (s *shell) Exit() {
 	s.stderr = nil
 }
 
-func streamReader(stream io.Reader, boundary string, buffer *string, signal *sync.WaitGroup) error {
-	// read all output until we have found our boundary token
-	output := ""
-	bufsize := 64
-	marker := boundary + newline
+func streamReader(stream io.Reader, boundary string, output chan<- string) {
 
+	// Read through the stream 1 line at a time
 	for {
-		buf := make([]byte, bufsize)
-		read, err := stream.Read(buf)
+		outbytes, err := readLine(stream)
 		if err != nil {
-			return err
-		}
-
-		output = output + string(buf[:read])
-
-		if strings.HasSuffix(output, marker) {
 			break
 		}
+		line := string(outbytes)
+		// we have a line if it is a boundry, end
+		if line == boundary {
+			close(output)
+			break
+		}
+		output <- line
 	}
+}
 
-	*buffer = strings.TrimSuffix(output, marker)
-	signal.Done()
-
-	return nil
+func readLine(stream io.Reader) ([]byte, error) {
+	buf := make([]byte, 1)
+	var outbytes []byte
+	for {
+		_, err := stream.Read(buf)
+		if err != nil {
+			return nil, err
+		}
+		if buf[0] == 13 {
+			// skip the '/r'
+			continue
+		}
+		if buf[0] == 10 {
+			return outbytes, nil
+		}
+		outbytes = append(outbytes, buf[0])
+	}
 }
 
 func createBoundary() string {
